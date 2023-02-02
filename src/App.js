632bb0bb3a595fc8337cca2162837a7b9adfb2de
Xing Chen <xchen601@gmail.com>
2023-02-01 19:31:23 -0500

    wooooo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function MyButton({ count, onClick }) {
+
+  return (
+    <button onClick={onClick}>
+      Click me | clicked {count} times
+    </button>
+  );
+}
 
 function App() {
+  const name = "Jeff"
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello <span style={{color:"red"}}>Jeff</span></h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+      <p>test123</p>
     </div>
   );
 }
